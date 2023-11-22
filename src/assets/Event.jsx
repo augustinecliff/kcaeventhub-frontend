94596c87e99f1c7b3355d19db61a1a2081bec359
augustine <augustinecliff10@gmail.com>
2023-11-22 17:04:43 +0300

    ticketGroup update - agastin v3.0

--- src/assets/Event.jsx
+++ src/assets/Event.jsx
@@ -8,7 +8,7 @@ export default function Event( {event} ) {
         },[event]);
     return (
             <div className="event hover w-full max-w-full rounded-2xl overflow-hidden p-2 bg-white" >
-                <div className="event-photo bg-cover bg-center transition-all rounded-xl">
+                <div className="event-photo bg-contain bg-no-repeat bg-center transition-all rounded-xl">
                 </div>
                 <p className="py-2 capitalize">{event.title}</p> {/*Amapiano tour Sep 2023*/}
                 <div className="flex flex-row justify-center items-center gap-2 text-sm font-medium p-0.2">
